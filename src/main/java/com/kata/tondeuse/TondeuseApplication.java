cdb415688b5ff941804f54357f12cbd0b25eb0a7
akilani <ameni.kilani@cpexterne.org>
2024-07-28 14:14:41 +0200

    add main class

--- src/main/java/com/kata/tondeuse/TondeuseApplication.java
+++ src/main/java/com/kata/tondeuse/TondeuseApplication.java
@@ -1,13 +1,31 @@
 package com.kata.tondeuse;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TondeuseApplication {
+public class TondeuseApplication implements CommandLineRunner {
+
+    @Autowired
+    private JobLauncher jobLauncher;
+
+    @Autowired
+    private Job job;
 
     public static void main(String[] args) {
         SpringApplication.run(TondeuseApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        JobExecution execution = jobLauncher.run(job, new JobParametersBuilder().toJobParameters());
+        System.out.println("Job Status : " + execution.getStatus());
+    }
 }
